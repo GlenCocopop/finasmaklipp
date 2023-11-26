966433aebd38328c0faee5a78fa05fe19f612dfc
GlenCocopop <126820343+GlenCocopop@users.noreply.github.com>
2023-11-26 01:08:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+
 function toggleInfo() {
     var moreInfo = document.getElementById("more-info");
     var infoText = document.getElementById("info-text");
-    
-    if (moreInfo.classList.contains("hidden")) {
-        moreInfo.classList.remove("hidden");
-        moreInfo.classList.add("expanded");
-        infoText.textContent = "Vad fan är det där?"; // Uppdatera texten om du vill ändra den efter klick
+    var infoBox = document.getElementById("info-box");
+
+    if (moreInfo.style.display === "none" || !moreInfo.style.display) {
+        moreInfo.style.display = "block"; // Visar mer information
+        infoBox.style.width = "80%"; // Expandera rutan
+        infoText.textContent = "Stäng"; // Ändra texten till "Stäng"
     } else {
-        moreInfo.classList.remove("expanded");
-        moreInfo.classList.add("hidden");
+        moreInfo.style.display = "none"; // Döljer informationen
+        infoBox.style.width = "auto"; // Återgå till mindre bredd
+        infoText.textContent = "Vad fan är det där?"; // Återställer texten
     }
 }
